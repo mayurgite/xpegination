2c9fae65a68a56b632677f80d3dc5b0509bc6978
Mayur Gite <mayurgite.it@gmail.com>
2024-02-10 15:38:45 +0530

    test

---
+++ src/compnent/App.jsx
@@ -0,0 +1,70 @@
+// src/components/Pagination.js
+
+import React, { useState, useEffect } from 'react';
+
+const Pagination = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, data.length);
+  const currentData = data.slice(startIndex, endIndex);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className='buttons'>
+        <button disabled={currentPage === 1} onClick={handlePrevPage}>Previous</button>
+        <p className='one'>{currentPage} </p>
+        <button disabled={currentPage === totalPages} onClick={handleNextPage}>Next</button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Pagination;
